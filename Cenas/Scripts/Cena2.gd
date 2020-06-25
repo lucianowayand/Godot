443abb8e484b047f6711aaaf7d7ecfbbafe72570
jshlw <JulioHenrique31501234@hotmail.com>
2020-06-25 19:07:51 -0300

    Evitar que ovos sejam instanciados próximos

--- Cenas/Scripts/Cena2.gd
+++ Cenas/Scripts/Cena2.gd
@@ -12,10 +12,21 @@ func _on_Intro_animation_finished(anim_name):
 	global_res.is_movable = true
 
 func instanciar_ovos(numero):
+	var ovos_instanciados = []
+	
 	for _i in range(numero):
 		var ovos = load("res://Instanciaveis/Enemies/Spider_sack/Spider_sack.tscn")
 		var ovos_instancias = ovos.instance()
 		ovos_instancias.set_name("SpiderSack"+String(numero))
+		
+		# evitar instanciar ovos proximos demais
 		randomize()
-		ovos_instancias.position = Vector2(rand_range(0,240),rand_range(190,270))
+		var vec = Vector2(rand_range(0,240),rand_range(190,270))
+		for i in range(ovos_instanciados.size()):
+			if vec.distance_squared_to(ovos_instanciados[i]) <= 1024:
+				# pegar outro Vector2 aleatorio
+				randomize()
+				vec = Vector2(rand_range(0,240),rand_range(190,270))
+				ovos_instancias.position = vec
 		add_child(ovos_instancias)
+		ovos_instanciados.append(ovos_instancias)
