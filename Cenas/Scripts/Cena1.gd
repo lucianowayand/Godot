a50e58bd1c037c3d7d6044ccaef4536c7a884266
lucianowayand <lucianowayand@hotmail.com>
2020-06-25 20:04:44 -0300

    Polimento do código

--- Cenas/Scripts/Cena1.gd
+++ Cenas/Scripts/Cena1.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
-func _on_Area2D_body_entered(body):
+func _on_Area2D_body_entered(body): #Quando o player entra na toca ele cai para a fase da aranha.
 	remove_child($Player)
 	get_tree().change_scene("res://Cenas/Cena2.tscn")
--- Cenas/Scripts/Cena2.gd
+++ Cenas/Scripts/Cena2.gd
@@ -1,21 +1,9 @@
 extends Node2D
 
-export var bottom = 270
-
-func _ready():
+func _ready(): #inicia o cinematic da queda, o player obviamente nao pode se mexer pra nao dar xibiu e os ovos sao gerados
 	$Intro.play("Intro")
 	global_res.is_movable = false
-	instanciar_ovos(4)
+	global_res.instanciar_em_area(Vector2(20,200),Vector2(200,250),4,"res://Instanciaveis/Enemies/Spider_sack/Spider_sack.tscn", "SpiderSack")
 	
-
-func _on_Intro_animation_finished(anim_name):
+func _on_Intro_animation_finished(_anim_name): #quando acaba o jogo se resume
 	global_res.is_movable = true
-
-func instanciar_ovos(numero):
-	for _i in range(numero):
-		var ovos = load("res://Instanciaveis/Enemies/Spider_sack/Spider_sack.tscn")
-		var ovos_instancias = ovos.instance()
-		ovos_instancias.set_name("SpiderSack"+String(numero))
-		randomize()
-		ovos_instancias.position = Vector2(rand_range(0,240),rand_range(190,270))
-		add_child(ovos_instancias)
