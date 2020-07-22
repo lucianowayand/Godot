a286a7d8f831ba903eb39cf754b78e452d910f70
Luciano <lucianowayand@hotmail.com>
2020-07-22 09:41:22 -0300

    Projeto descontinuado

--- Insaniquarium/Cenas/Aquarium.gd
+++ Insaniquarium/Cenas/Aquarium.gd
@@ -7,19 +7,25 @@ func _process(_delta):
 	$CanvasLayer/Label.set("text",String(global.coins))
 	$CanvasLayer/Label3.set("text",String(global.fishes))
 	
-#	if global.coins < 10:
-#		$CanvasLayer/Button.set("disabled", true)
-#	else:
-#		$CanvasLayer/Button.set("disabled", false)
-
-func _on_Button_pressed():
-	if global.fishes < 10:
-		global.instanciar_em_area($Spawn_Area/BL.position,$Spawn_Area/TR.position,1,"res://Fish/Fish.tscn","Guppy")
-		global.fishes+=1
-		global.coins -=10
-
 func _on_Coins_timeout():
 	global.coins += 1
 
 func _on_Trash_toggled(button_pressed):
 	global.trash_can = button_pressed
+
+func _on_Button_pressed():
+	if global.fishes < 10:
+		global.instanciar_em_area($Spawn_Area/BL.position,$Spawn_Area/TR.position,1,"res://Fish/Guppy/Guppy.tscn","Guppy")
+		global.fishes+=1
+		global.coins -=10
+		
+#	if global.coins < 10:
+#		$CanvasLayer/Button.set("disabled", true)
+#	else:
+#		$CanvasLayer/Button.set("disabled", false)
+
+func _on_Button2_pressed():
+	if global.fishes < 10:
+		global.instanciar_em_area($Spawn_Area/BL.position,$Spawn_Area/TR.position,1,"res://Fish/Reddy/Reddy.tscn","Reddy")
+		global.fishes+=1
+		global.coins -= 25
--- Insaniquarium/Fish/Fish.gd
+++ Insaniquarium/Fish/Fish.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 export (int) var fish_cost
 var moving = false
@@ -8,35 +8,48 @@ onready var dir = "null"
 onready var sides = get_parent().get_parent().get_child(1).get_node("Moving")
 onready var direcao
 var mouse_hover = false
+onready var state = "idle"
+onready var __name = "fish"
 
 func _ready():
 	if global.random(0,12) < 6:
 		dir = "Left"
 	else:
 		dir = "Right"
-		$Sprite.flip_h = true
+		sprite_flip_h()
 	
 	direcao = sides.get_node(dir)
+	$Animation.play("Nova Animação")
 
 func _process(_delta):
-	if position.x == direcao.position.x:
-		if dir == "Left":
-			dir = "Right"
-			direcao = sides.get_node(dir)
-			$Sprite.flip_h = true	
-		else:
-			dir = "Left"
-			direcao = sides.get_node(dir)
-			$Sprite.flip_h = false
-	position.x = move_toward(position.x,direcao.position.x, 1)
+	if state == "feeding":
+		pass
+	if state == "idle":
+		if position.x == direcao.position.x:
+			if dir == "Left":
+				dir = "Right"
+				direcao = sides.get_node(dir)
+				sprite_flip_h()
+			else:
+				dir = "Left"
+				direcao = sides.get_node(dir)
+				sprite_flip_h()
+		
+		position.x = move_toward(position.x,direcao.position.x, 1)
 
 	if Input.is_mouse_button_pressed(1) and mouse_hover and global.trash_can:
 		queue_free()
 		global.fishes -= 1
 		global.coins += 0.5 * value
+	
+func sprite_flip_h():
+	$Sprite.position.x = - $Sprite.position.x
+	$Sprite.scale.x = - $Sprite.scale.x 
 
-func _on_Area2D_mouse_entered():
-	mouse_hover = true
-
-func _on_Area2D_mouse_exited():
-	mouse_hover = false
+func _on_Food_area_entered(area):
+	var fish = area.get_parent()
+	print(fish.__name)
+	if fish.__name == "Guppy":
+		state == "feeding"
+		var dir = (fish.global_position - global_position).normalized()
+		move_and_collide(dir * 10)
