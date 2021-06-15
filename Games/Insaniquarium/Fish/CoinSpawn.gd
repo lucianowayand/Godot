bf0b531fef5335bbca114efdfcf629522b1832ee
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2021-06-15 10:45:36 -0300

    Reorganização de pastas
    Feita a remoção de jogos inacabados e organização de arquivos

--- Games/Insaniquarium/Fish/CoinSpawn.gd
+++
@@ -1,5 +0,0 @@
-extends "res://Fish/Timer.gd"
-
-func _on_Timer_timeout():
-	global.coins += 1
-	print("coins")
--- Games/Insaniquarium/Fish/Direction.gd
+++
@@ -1,5 +0,0 @@
-extends "res://Fish/Timer.gd"
-
-func _ready():
-	var dir = Vector2(rand_range(-1,1)*get_parent().speed,rand_range(-1,1)*get_parent().speed)
-	get_parent().translate(dir)
