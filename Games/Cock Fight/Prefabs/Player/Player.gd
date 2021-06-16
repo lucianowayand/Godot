115b3807750fb0bf13e62bf40c2846cec8eb06d9
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2021-06-16 15:44:41 -0300

    fixed animation modulator

--- Games/Cock Fight/Prefabs/Player/Player.gd
+++ Games/Cock Fight/Prefabs/Player/Player.gd
@@ -18,22 +18,30 @@ func _on_feet_body_exited(body):
 		is_touching_ground = false
 
 func _input(event):
-	if event.is_action_pressed("ui_right") or event.is_action_pressed("ui_left"):
-		$Sprite.animation = ("running")
-	else:
+	if event.is_action_pressed("debug"):
+		$Sprite.animation = ("punch")
+
+func _on_Sprite_animation_finished():
+	if $Sprite.animation == "punch":
 		$Sprite.animation = ("idle")
-		
+
 func _physics_process(_delta):
 	direction.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * speed
-	if (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) < 0:
-		$Sprite.flip_h = true
-	elif (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) > 0:
-		$Sprite.flip_h = false
+	if $Sprite.animation != "punch":
+		if (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) < 0:
+			$Sprite.animation = ("running")
+			$Sprite.flip_h = true
+		elif (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) > 0:
+			$Sprite.animation = ("running")
+			$Sprite.flip_h = false
+		else:
+			$Sprite.animation = ("idle")
 	
-	if Input.is_action_pressed("ui_up") and is_touching_ground:
-		direction.y =  -1 * jump_speed
-	
-	if !is_touching_ground:
+	if is_touching_ground:
+		if Input.is_action_pressed("ui_up"):
+			direction.y =  -1 * jump_speed
+	else:
 		direction.y +=  1 * GRAVITY
 
 	move_and_slide(direction)
+
